2f4f83bf0850983435b201bc9d41195fb4c7cbf1
gdharra2 <46692795+gdharra2@users.noreply.github.com>
2019-04-23 21:34:51 -0500

    Update Monte_Carlo_Simulation_Election.py

--- Monte_Carlo_Simulation_Election.py
+++ Monte_Carlo_Simulation_Election.py
@@ -1,165 +1,190 @@
-import csv
-import pickle
-import us
+import random
 import numpy as np
-from scipy.stats import norm
-
-try:
-    from numpy import random_intel as random  # Use Intel random if available
-except ImportError:
-    from numpy import random
-
-import pollster
-from pollster.rest import ApiException
-
-# Get state names from us.STATES
-states = {state.abbr: state.name for state in us.STATES}
-print('states',states)
-
-# Read electoral college votes from csv file
-# https://raw.githubusercontent.com/chris-taylor/USElection/master/data/electoral-college-votes.csv
-with open('electoral-college-votes.csv') as csvfile:
-    reader = csv.reader(csvfile)
-    college = {row[0]: int(row[1]) for row in reader}
-
-# Initialize Pollster API
-pollster = pollster.Api()
-
-cursor = None                # str | Special string to index into the Array (optional)
-tags = '2016-president'                # str | Comma-separated list of tag slugs. Only Charts with one or more of these tags and Charts based on Questions with one or more of these tags will be returned. (optional)
-election_date = '2016-11-01' # date | Date of an election
-
-try:
-    # Charts
-    charts = pollster.charts_get(cursor=cursor, tags=tags, election_date=election_date)
-    # print(charts)
-except ApiException as e:
-    print("Exception when calling DefaultApi->charts_get: %s\n" % e)
-# Save polls by pickle
-pickle.dump(charts, open('charts.p', 'wb'))
-
-question_slug = next(c.question.slug for c in charts.items if c.question.n_polls > 30)
-
-polls = pollster.polls_get(
-  question=question_slug,
-  sort='created_at'
-)
-#print(polls.items)
-
-state_polls = [questions
-               for poll in polls.items for questions in poll.poll_questions
-               if questions.question.name.partition(' Presidential')[0].replace('2016 ','').title() in states.values()]
-print(state_polls)
-
-for each_poll in state_polls:
-    each_poll.question.state = each_poll.question.name.partition(' Presidential')[0].replace('2016 ','').title()
-
-polls_by_state_counts = {state: sum(each_poll.question.state == state for each_poll in state_polls)
-                                    for state in states.values()}
-
-print(polls_by_state_counts)
+import pandas as pd
+import operator
 
 
-# Each state is a dictionary of arrays
-# obs: poll size
-# dem: Democrat popular vote percentage
-# rep: Republican popular vote percentage
-# other: Other popular vote percentage
-polls_by_state = {state: {'obs':[], 'dem':[], 'rep':[], 'other':[]} for state in states.values()}
-for question in state_polls:
-    state = question.question.state
-    obs = question.sample_subpopulations[0].observations
-    dem = ''
-    rep = ''
-    other = 0.0
-    for response in question.sample_subpopulations[0].responses:
-        if response.text == 'Clinton':
-            dem = response.value
-        if response.text == 'Trump':
-            rep = response.value
-        if response.text == 'Other':
-            other = response.value
-    # Check for empty responses
-    if state and obs and dem and rep:
-        polls_by_state[state]['obs'].append(obs)
-        polls_by_state[state]['dem'].append(dem)
-        polls_by_state[state]['rep'].append(rep)
-        polls_by_state[state]['other'].append(other)
+def create_candidates(list_of_candidates):
+    # code to create candidates
+    data = {}
+    for each_candidate in list_of_candidates:
+        data[each_candidate] = random.random()
 
-print(polls_by_state)
-
-state_mean_dem = {}
-state_std_dem = {}
-state_cdf_dem = {}
-
-state_mean_rep = {}
-state_std_rep = {}
-state_cdf_rep = {}
-
-for state, poll in polls_by_state.items():
-    dem = np.array(poll['dem'])
-    rep = np.array(poll['rep'])
-    other = np.array(poll['other'])
-    obs = np.array(poll['obs'])
-    dem_mean = 0.0
-    rep_mean = 0.0
-    other_mean = 0.0
-    if dem.size > 0:
-        dem_mean = np.average(dem, weights=obs)
-    if rep.size > 0:
-        rep_mean = np.average(rep, weights=obs)
-    if other.size > 0:
-        other_mean = np.average(other, weights=obs)
-
-    if dem_mean != 0.0 and rep_mean != 0.0:
-        state_mean_dem[state] = dem_mean / (dem_mean + rep_mean + other_mean) * 100
-
-        state_std_dem[state] = np.sqrt(np.average(np.square(dem - dem_mean) +
-                                                  (100 - dem) * dem / 10000, weights=obs)) / (dem_mean + rep_mean + other_mean) * 100
-
-        state_cdf_dem[state] = norm.cdf((state_mean_dem[state] - 50) / state_std_dem[state])
-
-        state_mean_rep[state] = rep_mean / (dem_mean + rep_mean + other_mean) * 100
-
-        state_std_rep[state] = np.sqrt(np.average(np.square(rep - rep_mean) +
-                                                  (100 - dem) * dem / 10000, weights=obs)) / (dem_mean + rep_mean + other_mean) * 100
-
-        state_cdf_rep[state] = norm.cdf((state_mean_rep[state] - 50) / state_std_rep[state])
-
-print(state_cdf_dem)
-print(state_cdf_rep)
-
-# Number of Monte Carlo simulations
-n_run = 100000
-random_numbers = random.rand(n_run, len(states))
-
-college_total = sum(college.values())  # Total electoral college votes
-college_required = college_total / 2  # Electoral college votes required to win
-college_dem = np.zeros(n_run)  # Democrats' total electoral college votes
-college_rep = np.zeros(n_run)
-
-# Magic
-for i, r in enumerate(random_numbers):
-    for state_index, state in enumerate(state_cdf_dem):
-        if r[state_index] < state_cdf_dem[state]:
-            college_dem[i] += college[state]
-        elif r[state_index] < state_cdf_rep[state]:
-            college_rep[i] += college[state]
-
-print('college_dem=',college_dem)
-print('college_rem=',college_rep)
-
-print('college_required=',college_required)
-
-chance_dem = sum(college_dem > college_required) / n_run * 100
-print(chance_dem)
-
-chance_rep = sum(college_rep > college_required) / n_run * 100
-print(chance_rep)
+    return data
 
 
-print('Median of Hillary Clinton\'s electoral votes: {}'.format(int(np.median(college_dem))))
-print('Median of Trump\'s electoral votes: {}'.format(int(np.median(college_rep))))
-print('Median of Other\'s electoral votes: {}'.format(int(college_total - np.median(college_dem) - np.median(college_rep))))
+def create_voters(number_of_voters, candidates):
+    # code to create voters
+    print(number_of_voters)
+    voters={'voter_id':[],'preferential_score_candidate_A':[],
+            'preferential_score_candidate_B':[],
+            'preferential_score_candidate_C': [],
+            'Vote':''}
+
+    fame_score_candidate_a = candidates['A']
+    fame_score_candidate_b = candidates['B']
+    fame_score_candidate_c = candidates['C']
+
+    for trial in range(0, number_of_voters):
+        voters['voter_id'].append(trial)
+        random_int = random.random()
+
+        voters['preferential_score_candidate_A'].append(1-abs(fame_score_candidate_a-random_int))
+        voters['preferential_score_candidate_B'].append(1-abs(fame_score_candidate_b-random_int))
+        voters['preferential_score_candidate_C'].append(1-abs(fame_score_candidate_c-random_int))
+
+    voters_pd = pd.DataFrame(voters)
+
+    return voters_pd
 
 
+def determine_winner(n_trials, voters):
+    # code to determine winner
+    total_score_a = voters.preferential_score_candidate_A.sum()/n_trials
+    total_score_b = voters.preferential_score_candidate_B.sum()/n_trials
+    total_score_c = voters.preferential_score_candidate_C.sum()/n_trials
+    print('total_score_A', total_score_a)
+    print('total_score_B', total_score_b)
+    print('total_score_C', total_score_c)
+
+    if total_score_a > total_score_b:
+        if total_score_a > total_score_c:
+            return 'A'
+        else:
+            return 'C'
+    else:
+        if total_score_b > total_score_c:
+            return 'B'
+        else:
+            return 'C'
+
+
+def generate_votes(voters):
+    voter_votes = []
+
+    for index, row in voters.iterrows():
+        print('Preferential Score Votes')
+        print('A', row.preferential_score_candidate_A)
+        print('B', row.preferential_score_candidate_B)
+        print('C', row.preferential_score_candidate_C)
+
+        max_value = max(row.preferential_score_candidate_A, row.preferential_score_candidate_B, row.preferential_score_candidate_C)
+        print('max_value=',max_value)
+        voted_candidate = 'A' if max_value == row.preferential_score_candidate_A else 'B' if max_value == row.preferential_score_candidate_B else 'C'
+        print('voted_candidate=', voted_candidate)
+        voter_votes.append(voted_candidate)
+
+    voters['Vote'] = voter_votes
+
+    return voters
+
+
+def generate_votes_based_on_candidates(voters, candidates_list):
+    voter_votes = []
+
+    for index, row in voters.iterrows():
+        print('Preferential Score Votes')
+        print('A', row.preferential_score_candidate_A)
+        print('B', row.preferential_score_candidate_B)
+        print('C', row.preferential_score_candidate_C)
+
+        list_of_preferential_scores = []
+
+        if 'A' in candidates_list:
+            list_of_preferential_scores.append(row.preferential_score_candidate_A)
+
+        if 'B' in candidates_list:
+            list_of_preferential_scores.append(row.preferential_score_candidate_B)
+
+        if 'C' in candidates_list:
+            list_of_preferential_scores.append(row.preferential_score_candidate_C)
+
+        max_value = max(list_of_preferential_scores)
+        print('max_value=',max_value)
+        voted_candidate = 'A' if max_value == row.preferential_score_candidate_A else 'B' if max_value == row.preferential_score_candidate_B else 'C'
+        print('voted_candidate=', voted_candidate)
+        voter_votes.append(voted_candidate)
+
+    voters['Vote'] = voter_votes
+
+    return voters
+
+
+def introduce_strategic_manipulation(num_strategic_voters, voters, candidate, total_voters):
+    get_famous_candidate = dict(sorted(candidate.items(), key=operator.itemgetter(1), reverse=True))
+    print(list(get_famous_candidate.keys())[0])
+    fame_candidate = list(get_famous_candidate.keys())[0]
+
+    for i in random.sample(range(0, total_voters), num_strategic_voters):
+        if voters.iat[i,4] != get_famous_candidate:
+            new_value = pd.DataFrame({'Vote': [fame_candidate]}, index=[i])
+            voters.update(new_value)
+
+    return voters
+
+
+def plurality_election_method(voters):
+    return voters.groupby('Vote')['Vote'].count().idxmax()
+
+
+def runoff_election_method(voters, number_of_candidates):
+    voters_by_count = voters.groupby('Vote')['Vote'].count() \
+                      .reset_index(name='count').sort_values('count', ascending=False)\
+                      .head(number_of_candidates-1)
+    print(voters_by_count)
+    if len(voters_by_count)==1:
+        return voters.groupby('Vote')['Vote'].count().idxmax()
+    else:
+        return 'B'
+
+
+def main():
+    # call all functions
+
+    # Number of Monte Carlo Simulations
+    n_run = 5
+
+    # Generate Candidates with fame score
+    candidates = create_candidates(['A', 'B', 'C'])
+    print('Candidates with fame score')
+    print(candidates)
+
+    # Generate Voters with preferential score for each candidate
+    voters = create_voters(n_run, candidates)
+    print('Voters with preferential score')
+    print(voters)
+
+    # Predict winner based on the preferential score
+    winner = determine_winner(n_run,voters)
+    print('Expected Winner = ',winner)
+
+    # Generate votes
+    voters = generate_votes(voters)
+    print('Votes Generated')
+    print(voters)
+
+    # Applying Strategic Manipulaiton
+    voters_strategy = introduce_strategic_manipulation(2, voters, candidates, n_run)
+    print('Strategic Manipulation')
+    print(voters_strategy)
+
+    # Get winner using Plurality
+    winner_plurality = plurality_election_method(voters)
+    print('Plurality Result = ', winner_plurality)
+
+    # Get winner using Plurality with strategy
+    winner_plurality_strategy = plurality_election_method(voters_strategy)
+    print('Strategy Plurality Result = ', winner_plurality_strategy)
+
+    # Get winner using runoff
+    winner_runoff = runoff_election_method(voters, len(candidates))
+    print('Run off Result =', winner_runoff)
+
+    voters_3 = generate_votes_based_on_candidates(voters, ['A','B','C'])
+    print('candidate based voters list')
+    print(voters_3)
+
+
+
+if __name__ == '__main__':
+    main()
